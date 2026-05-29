dfccc180b5821f52d1b05c1757924d29fc06e083
tinchosiara-png <tinchosiara@gmail.com>
2026-05-29 11:20:25 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'retrochat-v0-1-9';
+const CACHE = 'retrochat-v0-1-10';
 const ASSETS = ['./','./index.html','./manifest.json','./icon.svg'];
 
 self.addEventListener('install', e => {
