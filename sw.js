78462bd563e22505fe6accca408ded7183daa955
tinchosiara-png <tinchosiara@gmail.com>
2026-05-30 22:10:58 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'retrochat-v0-1-12b';
+const CACHE = 'retrochat-v0-1-12c';
 const ASSETS = ['./','./index.html','./manifest.json','./icon.svg'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
